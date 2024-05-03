dcb8b04066021cbb17ba033f8bc1e2d8b3a5050c
Osamuyi <osamuyia1@gmail.com>
2024-05-03 20:05:20 +0100

    Fixed documentation err

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-class Square:
-    """An empty class Square that defines a square."""
+class Square(object):
+    """
+    An empty class Square that defines a square.
 
-    def __init__(self):
-        """It initializes any new square instance that is created"""
+    Attributes:
+        None
+    """
 
-        pass
+    pass
