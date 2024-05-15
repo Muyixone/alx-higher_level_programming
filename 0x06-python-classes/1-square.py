6223aa1735c28a2a82c9498b4561efc9c7f00c46
Osamuyi <osamuyia1@gmail.com>
2024-05-15 18:02:34 +0100

    Added a private instance attribute to the Square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""A square class that defines a square"""
+
+
+class Square:
+    """A square class.
+
+    Args:
+        size(float): Determines the area of the square.
+
+    Attributes:
+        size(float): Determines the area of the square.
+
+    """
+
+    def __init__(self, size):
+        self.__size = size
