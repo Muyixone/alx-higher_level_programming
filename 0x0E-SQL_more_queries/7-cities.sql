a73146d968065936db43914f2f4f6c7c67643a5e
Osamuyi <osamuyia1@gmail.com>
2024-06-06 12:51:34 -0700

    Create a database and table with foreign key constraint

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Create a database and a table
+-- Creates the database 'hbtn_0d_usa' and the table 'cities'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	state_id INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id)
+	);
