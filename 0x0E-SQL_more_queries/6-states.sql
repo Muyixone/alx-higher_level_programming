9650084599e58c885240d0e4bc5b4bf6e3592c89
Osamuyi <osamuyia1@gmail.com>
2024-06-06 12:30:56 -0700

    Created a table with auto_generating ids

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- Create a database and a table
+-- Creates the database 'hbtn_0d_usa' and the table 'states'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
+	);
