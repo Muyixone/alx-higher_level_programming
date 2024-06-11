aecc51459638f8364e86ff37c5271fa59fb36bba
Osamuyi <osamuyia1@gmail.com>
2024-06-11 16:57:02 -0700

    Fixed error

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,6 +2,8 @@
 -- Creates the database 'hbtn_0d_usa' and the table 'cities'
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256) NOT NULL,
