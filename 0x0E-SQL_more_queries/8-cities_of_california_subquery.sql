1e27f1e44f1972890008f80b9407adc3642c260b
Osamuyi <osamuyia1@gmail.com>
2024-06-11 18:45:49 -0700

    Removed the state_id from the result

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,9 @@
 -- List all cities of California
 -- Subquery that lists id of states
-SELECT cities.*
+SELECT cities.id, cities.name
 FROM cities
 WHERE cities.state_id IN
-	( SELECT states.id
-	FROM states
+	( SELECT states.id FROM states
 	WHERE states.name = 'California'
 	)
 ORDER BY cities.id ASC;
