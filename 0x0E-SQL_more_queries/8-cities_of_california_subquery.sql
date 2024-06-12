6d35f818462fe0be392c37ceaab5179d8e36b72e
Osamuyi <osamuyia1@gmail.com>
2024-06-11 18:27:28 -0700

    changed id to state_id in the WHERE query

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- List all cities of California
 -- Subquery that lists id of states
 SELECT * FROM cities
-	WHERE id = ( SELECT id FROM states
+	WHERE state_id = ( SELECT id FROM states
 			WHERE name = 'California')
 ORDER BY cities.id ASC;
