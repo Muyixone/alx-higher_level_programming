2ef91077cca22fa1e0e2c0fb9c66be72a98f479e
Osamuyi <osamuyia1@gmail.com>
2024-06-17 13:02:02 -0700

    fixed a bug

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,7 +2,7 @@
 -- Displys NULL if a show doesn't have a genre
 SELECT
     tv_shows.title,
-    COALESCE(tv_show_genres.genre_id, 'NULL') AS genre_id
+    COALESCE(tv_show_genres.genre_id, NULL) AS genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.genre_id
