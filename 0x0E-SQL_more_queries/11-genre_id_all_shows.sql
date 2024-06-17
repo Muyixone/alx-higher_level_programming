c12b5ca49e08111aaae9c390c4cacc9e5268c518
Osamuyi <osamuyia1@gmail.com>
2024-06-17 12:57:01 -0700

    Used "LEFT JOIN" instead of "INNER JOIN"

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,8 +4,6 @@ SELECT
     tv_shows.title,
     COALESCE(tv_show_genres.genre_id, 'NULL') AS genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.genre_id
-INNER JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
