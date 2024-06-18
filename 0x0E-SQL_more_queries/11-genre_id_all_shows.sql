a2ad8b8333a6cc1443cfe500c3617d68ffefbd82
Osamuyi <osamuyia1@gmail.com>
2024-06-18 10:20:43 -0700

    fix

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,9 +1,7 @@
 -- Displays all shows present in the database
 -- Displys NULL if a show doesn't have a genre
-SELECT
-    tv_shows.title,
-    tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.genre_id
+    ON tv_shows.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
