a89669bcff79aeb89af3b0d895fd01d989a4d0b2
Osamuyi <osamuyia1@gmail.com>
2024-06-20 19:18:19 +0100

    Add check to the code

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,10 +5,10 @@ const [firstArg] = process.argv.splice(2);
 if (firstArg !== undefined) {
   const result = firstArg * 1;
 
-  if (isNaN(result)) {
-    console.log('Not a number');
-  } else {
+  if (isNaN(result) === false) {
     console.log(result);
+  } else {
+    console.log('Not a number');
   }
 } else {
   console.log('Not a number');
