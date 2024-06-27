fd8553633cc698fae7c3d0e847b26b9462337814
Osamuyi <osamuyia1@gmail.com>
2024-06-27 20:21:05 +0100

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (arguments.length !== 2 || w <= 0 || w === undefined || h <= 0 || h === undefined) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let str = '';
+
+      for (let j = 0; j < this.width; j++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
